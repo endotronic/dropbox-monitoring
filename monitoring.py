c4d8571caa826df35dd83df061ff72e30513e7dc
Kevin Wood <endotronic@gmail.com>
2021-11-28 01:00:40 +0000

    Seems ready

--- monitoring.py
+++ monitoring.py
@@ -21,29 +21,37 @@ class Metric(Enum):
 class State(Enum):
     STARTING = "starting"
     SYNCING = "syncing"
+    INDEXING = "indexing"
     UP_TO_DATE = "up to date"
+    SYNC_ERROR = "sync_error"
     NOT_RUNNING = "not running"
     UNKNOWN = "unknown"
 
 
 class DropboxInterface:
     """
-    This can be mocked for testing
+    This can be mocked for testing as needed
     """
 
     def __init__(self, logger: logging.Logger) -> None:
         self.logger = logger
 
     def query_status(self) -> Optional[str]:
-        result = subprocess.run(["dropbox", "status"], capture_output=True, text=True)
-        if result.stderr:
-            self.logger.warning("Dropbox status returned error: %s", result.stderr)
+        try:
+            result = subprocess.run(
+                ["dropbox", "status"], capture_output=True, text=True
+            )
+            if result.stderr:
+                self.logger.warning("Dropbox status returned error: %s", result.stderr)
+                return None
+            elif not result.stdout:
+                self.logger.warning("Dropbox status did not produce results")
+                return None
+            else:
+                return result.stdout
+        except:
+            self.logger.exception("Failed to invoke Dropbox")
             return None
-        elif not result.stdout:
-            self.logger.warning("Dropbox status did not produce results")
-            return None
-        else:
-            return result.stdout
 
 
 class DropboxMonitor:
@@ -58,18 +66,15 @@ class DropboxMonitor:
         self.min_poll_interval_sec = min_poll_interval_sec
         self.logger = logger
         self.prom_port = prom_port
-        self.status_matcher = re.compile("(Syncing|Downloading|Uploading) (\\d+) files")
+        self.status_matcher = re.compile(
+            "(Syncing|Downloading|Uploading|Indexing) (\\d+) files"
+        )
 
         self.last_query_time = 0
         self.num_syncing = None  # type: Optional[int]
         self.num_downloading = None  # type: Optional[int]
         self.num_uploading = None  # type: Optional[int]
-
-        self.status_enum = EnumMetric(
-            "dropbox_status",
-            "Status reported by Dropbox client",
-            states=[state.value for state in State.__members__.values()],
-        )
+        self.state = State.STARTING
 
         self.num_syncing_gauge = Gauge(
             "dropbox_num_syncing",
@@ -86,7 +91,14 @@ class DropboxMonitor:
             "Number of files currently uploading",
         )
 
+        self.status_enum = EnumMetric(
+            "dropbox_status",
+            "Status reported by Dropbox client",
+            states=[state.value for state in State.__members__.values()],
+        )
+
     def start(self) -> None:
+        self.status_enum.state(State.STARTING.value)
         self.num_syncing_gauge.set_function(
             partial(self.get_status, Metric.NUM_SYNCING)
         )
@@ -100,26 +112,26 @@ class DropboxMonitor:
         start_http_server(self.prom_port)
         self.logger.info("Started Prometheus server on port %d", self.prom_port)
 
-    def get_status(self, metric: Metric) -> Optional[int]:
+    def get_status(self, metric: Metric) -> int:
         now = time()
         if now - self.last_query_time > self.min_poll_interval_sec:
             dropbox_result = self.dropbox.query_status()
             if dropbox_result:
                 self.parse_output(dropbox_result)
             else:
-                self.status_enum.state(State.UNKNOWN)
+                self.status_enum.state(State.UNKNOWN.value)
                 self.num_syncing = None
                 self.num_downloading = None
                 self.num_uploading = None
 
         if metric == Metric.NUM_SYNCING:
-            return self.num_syncing
+            return self.num_syncing or 0
         elif metric == Metric.NUM_DOWNLOADING:
-            return self.num_downloading
+            return self.num_downloading or 0
         elif metric == Metric.NUM_UPLOADING:
-            return self.num_uploading
+            return self.num_uploading or 0
         else:
-            return None
+            raise ValueError(metric)
 
     def parse_output(self, results: str) -> None:
         """
@@ -127,8 +139,13 @@ class DropboxMonitor:
 
         Up to date
         Syncing...
+        Indexing...
         Syncing 176 files • 6 secs
         Downloading 176 files (6 secs)
+        Dropbox isn't running!
+        Indexing 1 file...
+        Can't sync "monitoring.txt" (access denied)
+        Syncing "none" • 1 sec
         """
         state = State.UNKNOWN
         num_syncing = None  # type: Optional[int]
@@ -142,12 +159,8 @@ class DropboxMonitor:
                     self.num_syncing = 0
                     self.num_downloading = 0
                     self.num_uploading = 0
-                    return
-                elif line == "Syncing...":
-                    state = State.SYNCING
-                    self.num_syncing = None
-                    self.num_downloading = None
-                    self.num_uploading = None
+                if line == "Dropbox isn't running!":
+                    state = State.NOT_RUNNING
                 else:
                     status_match = self.status_matcher.match(line)
                     if status_match:
@@ -160,7 +173,13 @@ class DropboxMonitor:
                             self.num_downloading = num_files
                         if action == "Uploading":
                             self.num_uploading = num_files
-                    elif line != "Syncing...":
+                    elif line.startswith("Syncing"):
+                        state = State.SYNCING
+                    elif line.startswith("Indexing"):
+                        state = State.INDEXING
+                    elif line.startswith("Can't sync"):
+                        state = State.SYNC_ERROR
+                    else:
                         self.logger.debug("Ignoring line '%s'", line)
             except:
                 self.logger.exception("Failed to parse status line '%s'", line)
@@ -193,14 +212,16 @@ if __name__ == "__main__":
 
     log_level = logging.getLevelName(args.log_level)
     global_log_level = logging.getLevelName(args.global_log_level)
-    logging.basicConfig(format="%(levelname)s: %(message)s", level=global_log_level)
+    logging.basicConfig(
+        format="[MONITORING %(levelname)s]: %(message)s", level=global_log_level
+    )
     logger = logging.getLogger("dropbox_monitor")
     logger.setLevel(log_level)
 
     dropbox = DropboxInterface(logger)
     monitor = DropboxMonitor(
         dropbox=dropbox,
-        min_poll_interval_sec=args.min_poll_interval_sec,
+        min_poll_interval_sec=int(args.min_poll_interval_sec),
         logger=logger,
         prom_port=args.port,
     )
